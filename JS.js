af9f25355afab46bb33fc85b7f68d9c484733f06
Meher Shiri <mehershiri28@gmail.com>
2024-12-01 18:45:59 +0530

    Update JS.js

--- JS.js
+++ JS.js
@@ -4,10 +4,8 @@ function updateMood() {
     const memeCharacter = document.getElementById('memeCharacter');
     const memeContainer = document.querySelector('.meme-container');
 
-    // Set default image
     let imageUrl = 'images/girlie.jpg';
 
-    // Change border color and image based on mood
     switch (mood) {
         case 'chill':
             imageUrl = 'images/chill.jpg';
@@ -19,43 +17,43 @@ function updateMood() {
             break;
         case 'sad':
             imageUrl = 'images/sad.jpg';
-            memeContainer.style.borderColor = '#87CEFA'; // Light blue
+            memeContainer.style.borderColor = '#87CEFA';
             break;
         case 'angry':
             imageUrl = 'images/angry.jpg';
-            memeContainer.style.borderColor = '#FF0000'; // Red
+            memeContainer.style.borderColor = '#FF0000';
             break;
         case 'anxious':
             imageUrl = 'images/anxious.png';
-            memeContainer.style.borderColor = '#F08080'; // Light coral
+            memeContainer.style.borderColor = '#F08080'; 
             break;
         case 'hungry':
             imageUrl = 'images/hungry.jpg';
-            memeContainer.style.borderColor = '#FF0000'; // Light coral
+            memeContainer.style.borderColor = '#FF0000';
             break;
         case 'stressed':
             imageUrl = 'images/stress.jpg';
-            memeContainer.style.borderColor = '#967BB6'; // Light coral
+            memeContainer.style.borderColor = '#967BB6'; 
             break;
         case 'cozy':
             imageUrl = 'images/cozy.jpg';
-            memeContainer.style.borderColor = '#E6E6FA'; // Light coral
+            memeContainer.style.borderColor = '#E6E6FA'; 
             break;
         case 'working':
             imageUrl = 'images/work.jpg';
-            memeContainer.style.borderColor = '#DCD0FF'; // Light coral
+            memeContainer.style.borderColor = '#DCD0FF';
             break;
         case 'broke':
             imageUrl = 'images/broke.jpg';
-            memeContainer.style.borderColor = '#87CEEB'; // Light coral
+            memeContainer.style.borderColor = '#87CEEB';
             break;
         default:
             imageUrl = 'images/girlie.png';
-            memeContainer.style.borderColor = '#9E5D8E'; // Default magenta-like color
+            memeContainer.style.borderColor = '#9E5D8E'; 
             break;
     }
 
-    // Update the CSS background image with the selected image
+
     memeCharacter.style.backgroundImage = `url('${imageUrl}')`;
 }
 document.addEventListener('DOMContentLoaded', () => {
